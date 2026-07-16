0326851b201da44c3200f009e0fa7ded736b69ca
Trash Can <kinetic.0212@gmail.com>
2026-07-17 02:49:56 +0530

    save uploaded files to AWS s3 bucket

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,160 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+// const maxVideoSize = 1 << 30
+
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	fmt.Println("uploading video", videoID, "by user", userID)
+
+	// TODO: implement the upload here
+
+	const maxMemory int64 = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't upload thumbnail", err)
+		return
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+	mediaType, params, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't get media type", err)
+		return
+	}
+	fmt.Println(params)
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "video type should be mp4", err)
+		return
+	}
+	mediaExtension := strings.Split(mediaType, "/")
+	/*
+		data, err := io.ReadAll(file)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "couldn't read file", err)
+			return
+		}
+	*/
+
+	metaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't get video info", err)
+		return
+	}
+
+	if metaData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "unauthorized video", err)
+		return
+	}
+
+	randomBytes := make([]byte, 32)
+	_, err = rand.Read(randomBytes)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't fill random bytes", err)
+		return
+	}
+
+	destFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't save file", err)
+		return
+	}
+	defer os.Remove(destFile.Name())
+	defer destFile.Close()
+
+	written, err := io.Copy(destFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't write file", err)
+		return
+	}
+
+	_, err = destFile.Seek(0, io.SeekStart)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't reset Tempfile's file pointer", err)
+		return
+	}
+
+	fmt.Println(written)
+
+	videoFile := base64.RawURLEncoding.EncodeToString(randomBytes) + "." + mediaExtension[1]
+	s3PutObjectOutput, err := cfg.s3Client.PutObject(context.Background(), &s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         &videoFile,
+		Body:        destFile,
+		ContentType: &mediaType,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't upload Tempfile to s3 bucket", err)
+		return
+	}
+
+	fmt.Println(s3PutObjectOutput.Size)
+	fmt.Println(s3PutObjectOutput.BucketKeyEnabled)
+	fmt.Println(s3PutObjectOutput.ETag)
+	fmt.Println(s3PutObjectOutput.ResultMetadata)
+
+	// thumbnailData := base64.StdEncoding.EncodeToString(data)
+	// thumbnailURL := fmt.Sprintf("data:%s;base64,%s", mediaType, thumbnailData)
+
+	/*
+		videoThumbnails[videoID] = thumbnail{
+			data:      data,
+			mediaType: mediaType,
+		}
+	*/
+
+	videoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, videoFile)
+	metaData.VideoURL = &videoURL
+	err = cfg.db.UpdateVideo(metaData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't update video metadata", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, database.Video{
+		ID:                metaData.ID,
+		CreatedAt:         metaData.CreatedAt,
+		UpdatedAt:         metaData.UpdatedAt,
+		ThumbnailURL:      metaData.ThumbnailURL,
+		VideoURL:          metaData.VideoURL,
+		CreateVideoParams: metaData.CreateVideoParams,
+	})
+
+}
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 
@@ -18,6 +21,7 @@ type apiConfig struct {
 	platform         string
 	filepathRoot     string
 	assetsRoot       string
+	s3Client         *s3.Client
 	s3Bucket         string
 	s3Region         string
 	s3CfDistribution string
@@ -83,6 +87,12 @@ func main() {
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
 	}
+	awsConfig, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(s3Region))
+	if err != nil {
+		log.Fatal("AWS_CONFIG is not initated")
+	}
+
+	s3Client := s3.NewFromConfig(awsConfig)
 
 	cfg := apiConfig{
 		db:               db,
@@ -94,6 +104,7 @@ func main() {
 		s3Region:         s3Region,
 		s3CfDistribution: s3CfDistribution,
 		port:             port,
+		s3Client:         s3Client,
 	}
 
 	err = cfg.ensureAssetsDir()
